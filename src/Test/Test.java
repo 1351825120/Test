506e41fdce5255f97d9cec15b0987b56d902d409
Jiawen9 <838982809@qq.com>
2021-08-02 21:00:31 +0800

    play
    play

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("hello word");
+		System.out.println("hello word");
+		System.out.println("hello word");
 	}
 
 }
